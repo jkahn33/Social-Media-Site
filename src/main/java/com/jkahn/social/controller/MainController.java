36f23e4050796c7338df98df462824ecb48e67c4
jgkahn33 <jgkahn33@gmail.com>
2018-03-20 13:00:39 -0400

    added spring webflow to the pom.xml

--- src/main/java/com/jkahn/social/controller/MainController.java
+++ src/main/java/com/jkahn/social/controller/MainController.java
@@ -4,11 +4,16 @@ import main.java.com.jkahn.social.objects.SentUser;
 import main.java.com.jkahn.social.objects.User;
 import main.java.com.jkahn.social.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
 import java.util.Calendar;
 import java.util.logging.Logger;
 
@@ -25,17 +30,18 @@ public class MainController {
     }
 
     @PostMapping(value = "/registerUser")
-    public void register(@RequestBody SentUser user, HttpServletResponse res, Model theModel){
-        log.info(user.getEmail());
+    public void register(@RequestBody SentUser user, HttpServletResponse res, Model theModel) throws java.io.IOException{
+
+        log.info(user.getFirst());
         Calendar c = Calendar.getInstance();
         User newUser = new User(user.getFirst(), user.getLast(), user.getEmail(), c.get(Calendar.MONTH)+1, c.get(Calendar.DAY_OF_MONTH), c.get(Calendar.YEAR));
         userService.addUser(newUser);
 
         theModel.addAttribute("user", user);
+        res.sendRedirect("/homepage");
     }
 
     @GetMapping("/homepage")
-    public String homepage(){
-
+    public void homepage(){
     }
 }
